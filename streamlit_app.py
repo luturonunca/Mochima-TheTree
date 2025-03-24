65e91fb955e2e12cf8f7bace1093d065df36e93f
Nunez Arturo <arturo.nunez@lam.fr>
2025-03-24 16:33:21 +0100

    bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,25 +19,4 @@ fig = px.treemap(
     title="Click to Explore the Simulation Lineage"
 )
 
-st.plotly_chart(fig, use_contaimport streamlit as st
-import plotly.express as px
-
-st.set_page_config(layout="wide", page_title="Galaxy Simulation Tree")
-
-st.title("Galaxy Simulation Evolution Tree")
-
-names = [
-    "Benchmark", 
-    "Benchmark/Star Formation", 
-    "Benchmark/Star Formation/SN Feedback", 
-    "Benchmark/Star Formation/SN Feedback/Protostellar", 
-    "Benchmark/Star Formation/SN Feedback/Protostellar/AGN"
-]
-
-fig = px.treemap(
-    names=names,
-    path=[names],
-    title="Click to Explore the Simulation Lineage"
-)
-
 st.plotly_chart(fig, use_container_width=True)
